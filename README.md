e909243fef518d7e5dc66c7af59bdf0b7301caa6
MaryamAlEisah <maryam.aleisah@gmail.com>
2021-04-27 03:10:52 +0300

    I finish lab-02

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lab-02
+
+My project name: About me
+
+I create it using CSS JS HTML ( .eslintrc.json/.gitignore/license/rest.css)
+
+I interact with users by asking them questions
---
+++ app.js
@@ -0,0 +1,83 @@
+'use strict';
+
+let userName = prompt('Hi, Could you please enter your name');
+console.log(userName);
+
+alert('userName' + 'Welcome to my page');
+
+
+ // I will ask user 5 yes/no Q
+
+
+let q1=prompt('Do you have a car?');
+switch(q1.toUpperCase()){
+  case 'yes': console.log('yes i have one');
+    alert('yes i have one');
+    break;
+    case 'no': console.log('no i do not have one');
+    alert('no i do not have one');
+    default:
+      console.log('please ask me again');
+      alert('please ask me again');
+ 
+}
+
+let q1=prompt('Do you like pizza ?');
+switch(q1.toUpperCase()){
+  case 'yes': console.log('yes i like it');
+    alert('yes i like it');
+    break;
+    case 'no': console.log('no i do not like it');
+    alert('no i do not like it');
+    default:
+      console.log('please ask me again');
+      alert('please ask me again');
+ 
+}
+
+
+let q1=prompt('Do you like reading?');
+switch(q1.toUpperCase()){
+  case 'yes': console.log('yes i like reading');
+    alert('yes i like reading');
+    break;
+    case 'no': console.log('no i do like reading');
+    alert('no i do not like reading');
+    default:
+      console.log('please ask me again');
+      alert('please ask me again');
+ 
+}
+
+
+let q1=prompt('Do you like my book?');
+switch(q1.toUpperCase()){
+  case 'yes': console.log('yes i do');
+    alert('yes i do');
+    break;
+    case 'no': console.log('no i do not ');
+    alert('no i do not');
+    default:
+      console.log('please ask me again');
+      alert('please ask me again');
+ 
+}
+
+let q1=prompt('Do you think I love coding?');
+switch(q1.toUpperCase()){
+  case 'yes': console.log('yes i do');
+    alert('yes i do');
+    break;
+    case 'no': console.log('no i do not');
+    alert('no i do not ');
+    default:
+      console.log('please ask me again');
+      alert('please ask me again');
+ 
+}
+
+
+
+
+
+
