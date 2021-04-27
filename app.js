56dd88c6165b0176e8cd7addc8fdb79044877d91
MaryamAlEisah <maryam.aleisah@gmail.com>
2021-04-27 15:04:33 +0300

    fix-lab-02

--- app.js
+++ app.js
@@ -3,76 +3,99 @@
 let userName = prompt('Hi, Could you please enter your name');
 console.log(userName);
 
-alert('userName' + 'Welcome to my page');
+alert(userName + 'Welcome to my page');
 
 
  // I will ask user 5 yes/no Q
 
 
-let q1=prompt('Do you have a car?');
-switch(q1.toUpperCase()){
-  case 'yes': console.log('yes i have one');
-    alert('yes i have one');
+let q1=prompt('Do I have a car?');
+switch(q1.toLowerCase()){
+  case 'yes': 
+  case 'y':
+    alert('no I do not have one');
+    console.log('yes I have one');
     break;
-    case 'no': console.log('no i do not have one');
-    alert('no i do not have one');
+    case 'no':
+    case 'n':
+    alert('you are correct');
+    console.log('no I have not');
+break;
     default:
       console.log('please ask me again');
-      alert('please ask me again');
+      alert('please enter yes or no');
  
 }
 
-let q1=prompt('Do you like pizza ?');
-switch(q1.toUpperCase()){
-  case 'yes': console.log('yes i like it');
-    alert('yes i like it');
+let q2=prompt('Do I like pasta ?');
+switch(q1.toLowerCase()){
+  case 'yes': 
+  case 'y' :
+    alert(' no I dont like it');
+    console.log('yes I like it');
     break;
-    case 'no': console.log('no i do not like it');
-    alert('no i do not like it');
+    case 'no': 
+    case 'n':
+    alert('you are correct');
+    console.log('no I do not like it');
+break;
     default:
       console.log('please ask me again');
-      alert('please ask me again');
- 
-}
-
-
-let q1=prompt('Do you like reading?');
-switch(q1.toUpperCase()){
-  case 'yes': console.log('yes i like reading');
-    alert('yes i like reading');
-    break;
-    case 'no': console.log('no i do like reading');
-    alert('no i do not like reading');
-    default:
-      console.log('please ask me again');
-      alert('please ask me again');
+      alert('please enter yes or no');
  
 }
 
 
-let q1=prompt('Do you like my book?');
-switch(q1.toUpperCase()){
-  case 'yes': console.log('yes i do');
-    alert('yes i do');
+let q3=prompt('Do I like reading newspaper?');
+switch(q1.toLowerCase()){
+  case 'yes': 
+  case 'y':
+   alert('no I do not');
+   console.log('yes I like reading newspaper');
     break;
-    case 'no': console.log('no i do not ');
-    alert('no i do not');
+    case 'no': 
+    case 'n' :
+    alert('you are correct');
+    console.log('no I do not like reading newspaper');
     default:
       console.log('please ask me again');
-      alert('please ask me again');
+      alert('enter yes or no');
  
 }
 
-let q1=prompt('Do you think I love coding?');
-switch(q1.toUpperCase()){
-  case 'yes': console.log('yes i do');
-    alert('yes i do');
+
+let q4=prompt('Do I like cooking ?');
+switch(q1.toLowerCase()){
+  case 'yes': 
+  case 'y' :
+    alert('no I do not');
+    console.log('yes I do');
     break;
-    case 'no': console.log('no i do not');
-    alert('no i do not ');
+    case 'no': 
+    case 'n':
+    alert('you are correct');
+    console.log('no I do not ');
+
     default:
       console.log('please ask me again');
-      alert('please ask me again');
+      alert('enter yes or no');
+ 
+}
+
+let q5=prompt('Do you think I like drawing?');
+switch(q1.toLowerCase()){
+  case 'yes': 
+  case 'y':
+   alert('no I do not');
+   console.log('yes I do');
+    break;
+    case 'no': 
+    case 'n':
+    alert('you are correct ');
+    console.log('no I do not');
+    default:
+      console.log('please ask me again');
+      alert('please enter yes or no');
  
 }
 
